834659ac487851ac3810c386853f7af5e7d6fcf6
SlimEmm <ashudave4444@gmail.com>
2025-04-23 16:36:18 +0530

    - routes updated

--- node/index.js
+++ node/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form data
 app.use(
   cors({
-    origin: "https://thegreatproducts.com",
+    origin: "https://thegreatproducts.com/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
