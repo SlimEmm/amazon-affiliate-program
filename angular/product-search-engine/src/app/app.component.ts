37db1819e5c3c0416f47d5b8b2737eaae8b83201
SlimEmm <ashudave4444@gmail.com>
2025-04-23 17:05:47 +0530

    - social link component

--- angular/product-search-engine/src/app/app.component.ts
+++ angular/product-search-engine/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterLinksComponent } from './footer-links/footer-links.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterLinksComponent],
+  imports: [RouterOutlet, FooterLinksComponent, SocialLinksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
