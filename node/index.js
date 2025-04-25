6d78a2dbaa30704da60250b15402736c85a6fd03
SlimEmm <ashudave4444@gmail.com>
2025-04-25 20:08:07 +0530

    - node updates

--- node/index.js
+++ node/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form data
 app.use(
   cors({
-    origin: "https://thegreatproducts.com/",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
